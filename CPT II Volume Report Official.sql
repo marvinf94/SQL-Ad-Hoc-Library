25a822225936b27c1a86bbbd6f3cce9db53aee5f
marvinf94 <85895098+marvinf94@users.noreply.github.com>
2022-11-19 20:49:32 -0500

    Update CPT II Volume Report Official.sql

--- CPT II Volume Report Official.sql
+++ CPT II Volume Report Official.sql
@@ -1,3 +1,6 @@
+use warehouse ;
+use database ;
+
 Select
 ods.ORG_ID,
 ods.SRC_RNDRG_PRVDR_NPI_NUM,
@@ -8,7 +11,7 @@ from ODS.CCLF_5_PT_B_PHYS ods
 LEFT JOIN INSIGHTS.PROFILE_LIST_PHYSICIAN_LAYUP pl
 on pl.physician_npi = 'npi_num|'|| ods.SRC_CLM_LINE_HCPCS_CD
 Where fk_bene_id in (select fk_patient_id from INSIGHTS.PATIENT_X_MONTH where attribution_type = 'as_is' and 
-                     attribution_curr_period_flag = true and month_cd = 'm-2021-09') and SRC_CLM_THRU_DT between '2020-08-01' 
+                     attribution_curr_period_flag = true) and SRC_CLM_THRU_DT between '2020-08-01' 
                      and'2021-09-30'
 and src_clm_line_HCPCS_CD in (
 '99497',
